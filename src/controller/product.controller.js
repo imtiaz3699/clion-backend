5f2eb06ab89a4d5df4ed9f34e6f52741af6255d1
Imtiaz Ahmed <imtiazali78652@gmail.com>
2025-01-30 10:40:54 +0500

    product addded

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -11,41 +11,47 @@ export const addProduct = async (req, res) => {
     category,
   } = req.body;
   if (!product_name) {
-    return apiErrorResponse(res,400,'Product name is required');
+    return apiErrorResponse(res, 400, "Product name is required");
   }
-  if(!user_id){
-    return apiErrorResponse(res,400,'User Id is required.');
+  if (!user_id) {
+    return apiErrorResponse(res, 400, "User Id is required.");
   }
   try {
     const product = await prisma.product.create({
-        data:{
-            product_name,
-            product_description,
-            product_images,
-            quantity,
-            product_condition,
-            price,
-            user_id,
-            category
-        },
-        select:{
-            product_name:true,
-        }
-    })
-    if(!product){
-        return apiErrorResponse(res,500,'Error creating product');
+      data: {
+        product_name,
+        product_description,
+        product_images,
+        quantity,
+        product_condition,
+        price,
+        user_id,
+        category,
+      },
+      select: {
+        product_name: true,
+      },
+    });
+    if (!product) {
+      return apiErrorResponse(res, 500, "Error creating product");
     }
-    if(product){
-        return apiSuccessResponse(res,200,'Product created successfully',product)
+    if (product) {
+      return apiSuccessResponse(
+        res,
+        200,
+        "Product created successfully",
+        product
+      );
     }
-  }catch (e){
+  } catch (e) {
     console.log(e);
-    return apiErrorResponse(res,500,'Error creating product');
+    return apiErrorResponse(res, 500, "Error creating product");
   }
 };
 
-const updateProduct = async (req,res) => {
- const {
+export const updateProduct = async (req, res) => {
+  const productId = req.params.id;
+  const {
     product_name,
     product_description,
     product_images,
@@ -54,5 +60,92 @@ const updateProduct = async (req,res) => {
     price,
     user_id,
     category,
- } = req.body; 
-}
+  } = req.body;
+  if (!productId) {
+    return apiErrorResponse(res, 400, "Product Id is required");
+  }
+  if (!product_name) {
+    return apiErrorResponse(res, 400, "Product Name is required.");
+  }
+  try {
+    const product = await prisma.product.update({
+      where: { product_id: productId, user_id: user_id },
+      data: {
+        product_name,
+        product_description,
+        product_images,
+        quantity,
+        product_condition,
+        price,
+        user_id,
+        category,
+      },
+      select: {
+        product_name: true,
+      },
+    });
+    if (!product.id) {
+      return apiErrorResponse(res, 400, "Product not found.");
+    }
+    if (product) {
+      return apiSuccessResponse(
+        res,
+        200,
+        "Product updated successfully",
+        product
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    apiErrorResponse(res, 500, "Error updating product");
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const productId = req.params.id;
+  if (!productId) {
+    return apiErrorResponse(res, 400, "Product Id is required.");
+  }
+  try {
+    const product = await prisma.product.delete({
+      where: { product_id: productId, user_id: req.user.id },
+    });
+    if (product) {
+      return apiSuccessResponse(res, 200, {}, "Product deleted successfully.");
+    }
+  } catch (error) {
+    console.log(error);
+    apiErrorResponse(res, 500, "Error deleting product");
+  }
+};
+export const getProducts = async (req, res) => {
+  const productId = req.query.id;
+  try {
+    const products = await prisma.product.findMany({
+      where: { user_id: req.user.id,id:productId },
+      select: {
+        product_name: true,
+        product_description: true,
+        product_images: true,
+        quantity: true,
+        product_condition: true,
+        price: true,
+        category: true,
+        },
+    });
+    if (!products) {
+      return apiErrorResponse(res, 400, "Products not found.");
+    }
+    if (products) {
+      return apiSuccessResponse(
+        res,
+        200,
+        "Products found successfully",
+        products
+      );
+    }
+  } catch (e) {
+    console.log(e);
+    return apiErrorResponse(res, 500, "Error getting products");
+  }
+};
---
+++ src/routes/product.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { addProduct,getProducts,updateProduct,deleteProduct  } from "../controller/product.controller.js";
+import {authenticateJWT} from "../middleware/authenticate.js";
+const router = express.Router();
+
+router.post('/create-product',authenticateJWT, addProduct);
+router.put('/update-product:id',authenticateJWT, updateProduct);
+router.get('/get-products',authenticateJWT,getProducts)
+router.delete('/delete-products',authenticateJWT,deleteProduct)
+export default router
